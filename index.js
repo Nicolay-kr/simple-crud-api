63a6d357efb811461639fe124f5527af7b9b9ca5
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2021-11-28 16:38:49 +0300

    feat: work with errors

--- index.js
+++ index.js
@@ -22,9 +22,11 @@ const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.end(data);
-      } else if (urlRequest.pathname.match(/^\/person\/\d+/)) {
-        let personId = urlRequest.pathname.match(/\d+/)[0];
-        let person = persons.find((person) => person.id === personId);
+      } else if (urlRequest.pathname.match(/^\/person\/\d|-|\w/g)) {
+        let urlArr = urlRequest.pathname.split("/");
+        let persId = urlArr[2];
+
+        let person = persons.find((person) => person.id === persId);
 
         if (person) {
           res.statusCode = 200;
@@ -34,7 +36,7 @@ const server = http.createServer((req, res) => {
           stream.on('error', (error) => console.log('Error', error.message));
         } else {
           res.statusCode = 404;
-          res.end(`user with id ${personId} isn't finded`);
+          res.end(`user with id ${persId} isn't finded`);
         }
         // });
       }
@@ -47,7 +49,7 @@ const server = http.createServer((req, res) => {
         res.statusCode = 201;
         res.setHeader('Content-Type', 'application/json');
         let newPerson = {
-          id: params.id,
+          id: uuidv4(),
           name: params.name,
           age: params.age,
           hobbies: params.hobbies.split(','),
@@ -60,46 +62,55 @@ const server = http.createServer((req, res) => {
         newPerson = JSON.stringify(newPerson);
         res.end(newPerson);
       });
+
     } else if (req.method == 'PUT') {
-      if (urlRequest.pathname.match(/^\/person\/\d+/)) {
-        let personId = urlRequest.pathname.match(/\d+/)[0];
-        let body = '';
-        req.on('data', (chunk) => (body += chunk.toString()));
-
-        req.on('end', (chunk) => {
-          let params = parse(body);
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-
-          let editPerson = {
-            id: personId,
-            name: params.name,
-            age: params.age,
-            hobbies: params.hobbies.split(','),
-          };
-          persons.forEach((person, index) => {
-            if (person.id === personId) {
-              persons[index] = editPerson;
-            }
+      if (urlRequest.pathname.match(/^\/person\/\d|-|\w/g)) {
+        let urlArr = urlRequest.pathname.split("/");
+        let persId = urlArr[2];
+        if(!persons.find(person=>person.id===persId)){
+          res.statusCode = 404;
+          res.end(`user with id ${persId} isn't finded`);
+        }else{
+          let body = '';
+          req.on('data', (chunk) => (body += chunk.toString()));
+  
+          req.on('end', (chunk) => {
+            let params = parse(body);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+  
+            let editPerson = {
+              id: persId,
+              name: params.name,
+              age: params.age,
+              hobbies: params.hobbies.split(','),
+            };
+            persons.forEach((person, index) => {
+              if (person.id === persId) {
+                persons[index] = editPerson;
+              }
+            });
+            persons = { persons: [...persons] };
+            persons = JSON.stringify(persons);
+            let writeStream = fs.createWriteStream('./db.json', { flags: 'w' });
+            writeStream.write(persons);
+            editPerson = JSON.stringify(editPerson);
+            res.end(editPerson);
           });
-          persons = { persons: [...persons] };
-          persons = JSON.stringify(persons);
-          let writeStream = fs.createWriteStream('./db.json', { flags: 'w' });
-          writeStream.write(persons);
-          editPerson = JSON.stringify(editPerson);
-          res.end(editPerson);
-        });
+        }
+    
       } else {
         res.statusCode = 400;
         res.end(`user's id invalid`);
       }
+
     } else if (req.method == 'DELETE') {
-      if (urlRequest.pathname.match(/^\/person\/\d+/)) {
-        let personId = urlRequest.pathname.match(/\d+/)[0];
+      if (urlRequest.pathname.match(/^\/person\/\d|-|\w/g)) {
+        let urlArr = urlRequest.pathname.split("/");
+        let personId = urlArr[2];
         let deletePerson = persons.find((person) => person.id === personId);
 
         if (deletePerson) {
-          // console.log('personId',persons.filter((person) => person.id !== personId));
           persons = persons.filter((person) => person.id !== personId);
           persons = { persons: [...persons] };
           persons = JSON.stringify(persons);
@@ -107,10 +118,8 @@ const server = http.createServer((req, res) => {
           writeStream.write(persons);
 
           res.statusCode = 204;
-          res.setHeader('Content-Type', 'application/json');
-          deletePerson = JSON.stringify(deletePerson);
-          res.end(deletePerson);
-          stream.on('error', (error) => console.log('Error', error.message));
+          res.setHeader('Content-Type', 'text/html');
+          res.end("<p>peerson was dremove</p>");
         } else {
           res.statusCode = 404;
           res.end(`user with id ${personId} isn't finded`);
